af962197cfc53f0cf254e4a3bf89ef891067f020
Scyu_ <scyu@scyu.dev>
2022-10-03 03:40:27 +0100

    chore: add max values

--- src/main/java/com/wynntils/hades/protocol/packets/client/HCPacketUpdateStatus.java
+++ src/main/java/com/wynntils/hades/protocol/packets/client/HCPacketUpdateStatus.java
@@ -12,15 +12,18 @@ public class HCPacketUpdateStatus implements HadesPacket<IHadesServerAdapter> {
 
     double x, y, z;
     double health, mana;
+    double maxHealth, maxMana;
 
     public HCPacketUpdateStatus() { }
 
-    public HCPacketUpdateStatus(double x, double y, double z, double health, double mana) {
+    public HCPacketUpdateStatus(double x, double y, double z, double health, double maxHealth, double mana, double maxMana) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.health = health;
+        this.maxHealth = maxHealth;
         this.mana = mana;
+        this.maxMana = maxMana;
     }
 
     public double getX() {
@@ -43,13 +46,23 @@ public class HCPacketUpdateStatus implements HadesPacket<IHadesServerAdapter> {
         return mana;
     }
 
+    public double getMaxHealth() {
+        return maxHealth;
+    }
+
+    public double getMaxMana() {
+        return maxMana;
+    }
+
     @Override
     public void readData(HadesBuffer buffer) {
         x = buffer.readDouble();
         y = buffer.readDouble();
         z = buffer.readDouble();
         health = buffer.readDouble();
+        maxHealth = buffer.readDouble();
         mana = buffer.readDouble();
+        maxMana = buffer.readDouble();
     }
 
     @Override
@@ -58,7 +71,9 @@ public class HCPacketUpdateStatus implements HadesPacket<IHadesServerAdapter> {
         buffer.writeDouble(y);
         buffer.writeDouble(z);
         buffer.writeDouble(health);
+        buffer.writeDouble(maxHealth);
         buffer.writeDouble(mana);
+        buffer.writeDouble(maxMana);
     }
 
     @Override
