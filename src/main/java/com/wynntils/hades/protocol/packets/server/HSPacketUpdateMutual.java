a34a08366ff05376be6fc9b6e46bc806dde9a387
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-10-21 17:29:16 +0200

    feat: HSPacketUpdateMutual packet contains more information

--- src/main/java/com/wynntils/hades/protocol/packets/server/HSPacketUpdateMutual.java
+++ src/main/java/com/wynntils/hades/protocol/packets/server/HSPacketUpdateMutual.java
@@ -10,27 +10,40 @@ import java.util.UUID;
  * Used for updating mutual friends/party users their status.
  */
 public class HSPacketUpdateMutual implements HadesPacket<IHadesClientAdapter> {
-
     UUID user;
+    String name;
     double x, y, z;
     double health, mana;
     double maxHealth, maxMana;
+    String world;
+    boolean isMutualFriend;
+    boolean isMutualPartyMember;
+    boolean isMutualGuildMember;
 
     public HSPacketUpdateMutual() { }
 
-    public HSPacketUpdateMutual(UUID user, double x, double y, double z, double health, double maxHealth, double mana, double maxMana) {
+    public HSPacketUpdateMutual(UUID user, String name, double x, double y, double z, double health, double mana, double maxHealth, double maxMana, String world, boolean isMutualFriend, boolean isMutualPartyMember, boolean isMutualGuildMember) {
         this.user = user;
+        this.name = name;
         this.x = x;
         this.y = y;
         this.z = z;
         this.health = health;
-        this.maxHealth = maxHealth;
         this.mana = mana;
+        this.maxHealth = maxHealth;
         this.maxMana = maxMana;
+        this.world = world;
+        this.isMutualFriend = isMutualFriend;
+        this.isMutualPartyMember = isMutualPartyMember;
+        this.isMutualGuildMember = isMutualGuildMember;
     }
 
     public UUID getUser() {
         return user;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public double getX() {
@@ -61,9 +74,26 @@ public class HSPacketUpdateMutual implements HadesPacket<IHadesClientAdapter> {
         return maxMana;
     }
 
+    public String getWorld() {
+        return world;
+    }
+
+    public boolean isMutualFriend() {
+        return isMutualFriend;
+    }
+
+    public boolean isMutualGuildMember() {
+        return isMutualGuildMember;
+    }
+
+    public boolean isMutualPartyMember() {
+        return isMutualPartyMember;
+    }
+
     @Override
     public void readData(HadesBuffer buffer) {
         user = buffer.readUUID();
+        name = buffer.readString();
         x = buffer.readDouble();
         y = buffer.readDouble();
         z = buffer.readDouble();
@@ -71,11 +101,16 @@ public class HSPacketUpdateMutual implements HadesPacket<IHadesClientAdapter> {
         maxHealth = buffer.readDouble();
         mana = buffer.readDouble();
         maxMana = buffer.readDouble();
+        world = buffer.readString();
+        isMutualFriend = buffer.readBoolean();
+        isMutualPartyMember = buffer.readBoolean();
+        isMutualGuildMember = buffer.readBoolean();
     }
 
     @Override
     public void writeData(HadesBuffer buffer) {
         buffer.writeUUID(user);
+        buffer.writeString(name);
         buffer.writeDouble(x);
         buffer.writeDouble(y);
         buffer.writeDouble(z);
@@ -83,6 +118,10 @@ public class HSPacketUpdateMutual implements HadesPacket<IHadesClientAdapter> {
         buffer.writeDouble(maxHealth);
         buffer.writeDouble(mana);
         buffer.writeDouble(maxMana);
+        buffer.writeString(world);
+        buffer.writeBoolean(isMutualFriend);
+        buffer.writeBoolean(isMutualPartyMember);
+        buffer.writeBoolean(isMutualGuildMember);
     }
 
     @Override
