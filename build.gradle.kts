9ddc167dd311639c74ee3753e4fbc1919d6b71c6
Jamie <JamCal117@gmail.com>
2026-05-15 14:38:06 +0100

    chore: Bump to 0.6.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.wynntils.hades"
-version = "0.6.2"
+version = "0.6.3"
 
 repositories {
     mavenCentral()
--- src/main/java/com/wynntils/hades/protocol/enums/HadesVersion.java
+++ src/main/java/com/wynntils/hades/protocol/enums/HadesVersion.java
@@ -3,5 +3,6 @@ package com.wynntils.hades.protocol.enums;
 public enum HadesVersion {
     UNKNOWN,
     VERSION_0_6_1, // Gear sharing support with the introduction of versioning
-    VERSION_0_6_2  // Gear updates decoupled from position/vitals via HCPacketGearUpdate
+    // 0.6.2 was only library updates, fully compatible with 0.6.1
+    VERSION_0_6_3  // Gear updates decoupled from position/vitals via HCPacketGearUpdate
 }
